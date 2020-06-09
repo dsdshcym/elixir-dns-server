3aea90f4dd9bab4ffbdd003cc4cc22d9f7c0b0e9
Yiming Chen <dsdshcym@gmail.com>
2020-06-09 22:35:25 +0800

    parses question section
    Squashed commit of the following:
    
    commit d2a408bc82c6f30894d46777063f581de466eaa0
    Author: Yiming Chen <dsdshcym@gmail.com>
    
        use `with` syntax in `parse`
    
    commit a3b40f1013a1d4513e0688ed8f8187f6f02f0f12
    Author: Yiming Chen <dsdshcym@gmail.com>
    
        reformat parse_header pattern
    
    commit 63da174c5d3bb3eea58cc6cb63b11d8a548420b4
    Author: Yiming Chen <dsdshcym@gmail.com>
    
        extract parse_header helper function
    
    commit ce841b14b424c7f53210d3943ff9f9ad056d4bda
    Author: Yiming Chen <dsdshcym@gmail.com>
    
        parses multiple questions
    
    commit 2c2ce7a5dad6af8c632c3881e9d2f5f116130e35
    Author: Yiming Chen <dsdshcym@gmail.com>
    
        parses one question
    
    commit ed6017778aa688e9c75c38dfba66c76585551fa1
    Author: Yiming Chen <dsdshcym@gmail.com>
    
        parses header

--- lib/dns/packet.ex
+++ lib/dns/packet.ex
@@ -1,11 +1,32 @@
 defmodule DNS.Packet do
-  def parse(<<header::binary-size(12), _rest::binary>>) do
-    <<id::size(16), qr::size(1), opcode::size(4), aa::size(1), tc::size(1), rd::size(1),
-      ra::size(1), z::size(3), rcode::size(4), qdcount::size(16), ancount::size(16),
-      nscount::size(16), arcount::size(16)>> = header
+  def parse(binary) do
+    with [header, rest] = parse_header(binary),
+         [questions, _rest] = parse_questions(rest, header.question_count) do
+      %{
+        header: header,
+        questions: questions
+      }
+    end
+  end
 
-    %{
-      header: %{
+  defp parse_header(<<
+         id::size(16),
+         qr::size(1),
+         opcode::size(4),
+         aa::size(1),
+         tc::size(1),
+         rd::size(1),
+         ra::size(1),
+         z::size(3),
+         rcode::size(4),
+         qdcount::size(16),
+         ancount::size(16),
+         nscount::size(16),
+         arcount::size(16),
+         rest::binary
+       >>) do
+    [
+      %{
         id: id,
         query_response: qr == 1,
         operation_code: opcode,
@@ -19,7 +40,41 @@ defmodule DNS.Packet do
         answer_count: ancount,
         authority_count: nscount,
         additional_count: arcount
-      }
-    }
+      },
+      rest
+    ]
+  end
+
+  defp parse_questions(binary, count, questions \\ [])
+
+  defp parse_questions(rest, 0, questions) do
+    [Enum.reverse(questions), rest]
+  end
+
+  defp parse_questions(binary, count, questions) do
+    [question, rest] = parse_question(binary)
+    parse_questions(rest, count - 1, [question | questions])
+  end
+
+  defp parse_question(binary) do
+    [label, rest] = extract_label(binary)
+    <<type::size(16), _class::size(16), rest::binary>> = rest
+
+    type =
+      case type do
+        1 -> :A
+      end
+
+    [%{name: label, type: type}, rest]
+  end
+
+  defp extract_label(binary, label_parts \\ [])
+
+  defp extract_label(<<0, rest::binary>>, label_parts) do
+    [label_parts |> Enum.reverse() |> Enum.join("."), rest]
+  end
+
+  defp extract_label(<<len::size(8), label_part::binary-size(len), rest::binary>>, label_parts) do
+    extract_label(rest, [label_part | label_parts])
   end
 end
